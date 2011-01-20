c1b21b8a5b7042055a51096b992f919f01fd740d
Your name <youremail@example.com>
2011-01-20 16:34:30 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatApp::Application.config.secret_token = '39ba4bb83652bc882da71aea16af87e3c2ff981a1dae4832fe264e3e93940b19e73218c4b63e0ff14c8037254e097d64bb79c3ddccb452cc9f7edd6c7c14a19c'
